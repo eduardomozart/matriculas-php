4280be0b3ef493e49876891d52e5ab4bfc24dc71
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-09-15 21:42:02 -0300

    Create cursos.php

---
+++ cursos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Título da página</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Cursos</h1>
+    
+    <h2>Cursos cadastrados</h2>
+
+    <h2>Cadastrar novo curso</h2>
+    <form action="curso_add.php" method="get">
+        <label for="nome_curso">Nome do curso:</label>
+        <input type="text" id="nome_curso" name="nome_curso">
+        <br>
+        <input type="submit">
+    </form>
+  </body>
+</html>
